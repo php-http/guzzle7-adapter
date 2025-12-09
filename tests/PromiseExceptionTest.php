bcd99429e091191d2defbaf9fb944dbf85ed4c70
Christopher Georg <christopher.georg@sr-travel.de>
2025-12-09 13:07:38 +0100

    ci: add tests for PHP 8.5

--- tests/PromiseExceptionTest.php
+++ tests/PromiseExceptionTest.php
@@ -40,8 +40,8 @@ final class PromiseExceptionTest extends TestCase
 
     public static function exceptionThatIsThrownForGuzzleExceptionProvider(): array
     {
-        $request = (new PromiseExceptionTest)->getMockBuilder(RequestInterface::class)->getMock();
-        $response = (new PromiseExceptionTest)->getMockBuilder(ResponseInterface::class)->getMock();
+        $request = (new PromiseExceptionTest('request'))->getMockBuilder(RequestInterface::class)->getMock();
+        $response = (new PromiseExceptionTest('response'))->getMockBuilder(ResponseInterface::class)->getMock();
 
         return [
             [$request, new GuzzleExceptions\ConnectException('foo', $request), NetworkException::class],
