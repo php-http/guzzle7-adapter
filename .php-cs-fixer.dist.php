559b141178a06bafba2c50f04b5becaa9ebc1099
Christopher Georg <christopher.georg@sr-travel.de>
2025-12-09 14:35:23 +0100

    ci: add tests for PHP 8.5

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -9,4 +9,5 @@ return $config->setFinder($finder)
     ->setRules([
          '@Symfony' => true,
          'array_syntax' => ['syntax' => 'short'],
+         'no_trailing_comma_in_singleline' => ['array', 'array_destructuring', 'group_import'],
     ]);
